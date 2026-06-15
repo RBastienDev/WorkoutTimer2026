931fe2c847ebd23387b5496ccdd3ebe6a7882b2d
Robert B <aliceinchainsistayaway@gmail.com>
2026-06-15 08:23:21 -0400

    added TimerManager Class

---
+++ WorkoutTimer2026 Watch App/TimerManager.swift
@@ -0,0 +1,16 @@
+//
+//  TimerManager.swift
+//  WorkoutTimer2026 Watch App
+//
+//  Created by Robert Bastien on 2026-06-15.
+//
+
+import Foundation
+import Combine
+import UserNotifications
+import WatchKit
+
+@MainActor
+final class TimerManager: ObservableObject {
+	
+}
